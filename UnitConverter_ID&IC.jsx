245ced46936c4749ae084b467b4195482613d055
Luis <luisfernandobrito@me.com>
2013-12-20 11:44:27 -0200

    code refactoring
    Code refactoring

--- UnitConverter_ID&IC.jsx
+++ UnitConverter_ID&IC.jsx
@@ -1,94 +1,109 @@
-﻿#targetengine "session" 
+﻿#targetengine "session"
 /**
-* @@@BUILDINFO@@@ UnitConverter_ID&IC.jsx !Version! Thu May 16 2013 16:58:48 GMT-0300
-* @ autor : Luis Fernando Brito
-*/
-
-
-var win = new Window("palette", 'Conversor de unidade InDesign e InCopy', [0,0,325,125], );
-with(win){
-	win.eTextDeUnit = add( "edittext", [185,25,310,47], undefined  );
-	win.sText = add( "statictext", [15,30,75,50], 'De:' );
-	win.sText = add( "statictext", [15,60,75,80], 'Para:' );
-	win.eTextParaUnit = add( "edittext", [185,55,310,77], undefined );
-	win.deUnit = add( "dropdownlist", [80,25,175,47], undefined, {items: ['Pixel: ','Polegadas:','Milímetros: ','Centímetros:','Pontos: ','Picas:','Cíceros:'], } );
-    win.deUnit.selection = 0;
-	win.paraUnit = add( "dropdownlist", [80,55,175,77], undefined, {items: ['Pixel: ','Polegadas:','Milímetros: ','Centímetros:','Pontos: ','Picas:','Cíceros:'], } );
-    win.paraUnit.selection = 0;
-    win.sText = add( "statictext", [205,112,320,132], 'powerby lbrito 2013' );
+ * @@@BUILDINFO@@@ UnitConverter_ID&IC.jsx !Version! Fri Dec 20 2013 11:41:31 GMT-0200
+ * @ autor : Luis Fernando Brito
+ */
+var win = new Window("palette", 'Conversor de unidade InDesign e InCopy', [0, 0, 325, 125], );
+with(win)
+{
+	win.eTextDeUnit = add("edittext", [185, 25, 310, 47], undefined);
+	win.sText = add("statictext", [15, 30, 75, 50], 'De:');
+	win.sText = add("statictext", [15, 60, 75, 80], 'Para:');
+	win.eTextParaUnit = add("edittext", [185, 55, 310, 77], undefined);
+	win.deUnit = add("dropdownlist", [80, 25, 175, 47], undefined,
+	{
+		items: ['Pixel: ', 'Polegadas:', 'Milímetros: ', 'Centímetros:', 'Pontos: ', 'Picas:', 'Cíceros:'],
+	});
+	win.deUnit.selection = 0;
+	win.paraUnit = add("dropdownlist", [80, 55, 175, 77], undefined,
+	{
+		items: ['Pixel: ', 'Polegadas:', 'Milímetros: ', 'Centímetros:', 'Pontos: ', 'Picas:', 'Cíceros:'],
+	});
+	win.paraUnit.selection = 0;
+	win.sText = add("statictext", [205, 112, 320, 132], 'powerby lbrito 2013');
 	win.sText.graphics.font = ScriptUI.newFont(win.sText.graphics.font.name, 'Bold', 10);
-	win.btnConverter = add( "button", [240,90,310,112], 'Converter' );
-    win.btnConverter.onClick = function () {win.eTextParaUnit.text = loadConvertUnists() }
+	win.btnConverter = add("button", [240, 90, 310, 112], 'Converter');
+	win.btnConverter.onClick = function ()
+	{
+		win.eTextParaUnit.text = loadConvertUnists()
 	}
-
+}
 win.center();
 win.show();
 
-function loadConvertUnists(){
-        var converterDe = Number(win.eTextDeUnit.text);
-        var unidade = selectItem ()[0];
-        var converterPara = selectItem ()[1];    
-    return convertUnits(converterDe+""+unidade,converterPara);
+function loadConvertUnists()
+{
+	var converterDe = Number(win.eTextDeUnit.text);
+	var unidade = selectItem()[0];
+	var converterPara = selectItem()[1];
+	return convertUnits(converterDe + "" + unidade, converterPara);
 }
 
-function convertUnits(converterDe,converterPara){
-    var UnidadeDeValor = new UnitValue(converterDe);
-    if (UnidadeDeValor.type == "?")
-    {
-        $.writeln(" Tipo de unidade desconhecida.");
-        beep()
-    }
-    else
-    {
-         UnidadeDeValor.convert(converterPara);
-    }
-    return UnidadeDeValor;   
+function convertUnits(converterDe, converterPara)
+{
+	var UnidadeDeValor = new UnitValue(converterDe);
+	if (UnidadeDeValor.type == "?")
+	{
+		$.writeln(" Tipo de unidade desconhecida.");
+		beep()
+	}
+	else
+	{
+		UnidadeDeValor.convert(converterPara);
+	}
+	return UnidadeDeValor;
 }
 
-
-function selectItem(){
-    if(win.deUnit.selection == 0){
-        var de = "px";
-    }    
-    if(win.deUnit.selection == 1){
-        var de = "in";
-    }
-    if(win.deUnit.selection == 2){
-        var de = "mm";
-    }    
-    if(win.deUnit.selection == 3){
-        var de = "cm";
-    }
-    if(win.deUnit.selection == 4){
-        var de = "pt";
-    }
-    if(win.deUnit.selection == 5){
-        var de = "pica";
-    }    
-    if(win.deUnit.selection == 6){
-        var de = "cicero";
-    }
-    if(win.paraUnit.selection == 0){
-        var para = "px";
-    }    
-    if(win.paraUnit.selection == 1){
-        var para = "in";
-    }
-    if(win.paraUnit.selection == 2){
-        var para = "mm";
-    }    
-    if(win.paraUnit.selection == 3){
-        var para = "cm";
-    }
-    if(win.paraUnit.selection == 4){
-        var para = "pt";
-    }
-    if(win.paraUnit.selection == 5){
-        var para = "pica";
-    }    
-    if(win.paraUnit.selection == 6){
-        var para = "cicero";
-    }
-    var selectUnit = [de,para];
-return selectUnit;
-}    
+function selectItem()
+{
+	switch (win.deUnit.selection.valueOf())
+	{
+	case 0:
+		var de = "px";
+		break;
+	case 1:
+		var de = "in";
+		break;
+	case 2:
+		var de = "mm";
+		break;
+	case 3:
+		var de = "cm";
+		break;
+	case 4:
+		var de = "pt";
+		break;
+	case 5:
+		var de = "pica";
+		break;
+	case 6:
+		var de = "cicero";
+		break;
+	}
+	switch (win.paraUnit.selection.valueOf())
+	{
+	case 0:
+		var para = "px";
+		break;
+	case 1:
+		var para = "in";
+		break;
+	case 2:
+		var para = "mm";
+		break;
+	case 3:
+		var para = "cm";
+		break;
+	case 4:
+		var para = "pt";
+		break;
+	case 5:
+		var para = "pica";
+		break;
+	case 6:
+		var para = "cicero";
+		break;
+	}
+	var selectUnit = [de, para];
+	return selectUnit;
+}
